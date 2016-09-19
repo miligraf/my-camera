31ad74c1d16b116359b127919f04c2806312278d
Milton Andrade <miligraf@gmail.com>
2016-09-19 12:19:00 -0400

    add cordovaCamera and ability to take picture and display it

--- www/js/app.js
+++ www/js/app.js
@@ -2,13 +2,23 @@
 
   var app = angular.module('starter', ['ionic', 'ngCordova']);
 
-  app.controller('CameraCtrl', function($scope) {
+  app.controller('CameraCtrl', function($scope, $cordovaCamera) {
     $scope.pictureUrl = 'http://placehold.it/300x300';
 
     $scope.takePicture = function() {
-
+      var options = {
+        destinationType: Camera.DestinationType.DATA_URL,
+        encodingType: Camera.EncodingType.JPEG
+      }
+      $cordovaCamera.getPicture(options)
+        .then(function(data) {
+          console.log('camera data: ' + angular.toJson(data));
+          $scope.pictureUrl = "data:image/jpeg;base64," + data;
+        }, function(error) {
+          console.log('camera error: ' + angular.toJson(data));
+        });
     };
-    
+
   });
 
   app.run(function($ionicPlatform) {
